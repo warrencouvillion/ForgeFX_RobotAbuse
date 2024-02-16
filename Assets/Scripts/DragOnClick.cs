ea3b2e9df81e88b54f15ed89b73173285e19bf31
warrencouvillion <31316773+warrencouvillion@users.noreply.github.com>
2024-02-16 10:35:30 -0600

    Arm now reattaches when close to socket.

--- Assets/Scripts/DragOnClick.cs
+++ Assets/Scripts/DragOnClick.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class DragOnClick : MonoBehaviour
 {
+    public float m_attachDistance = 0.1f;
     public float m_mouseMovementScale = 0.001f;
     private Transform m_groupRootXform;
+    private Transform m_parentXform;
     private Vector3 m_anchorLocalPosition;
-    private Quaternion m_anchorLocalRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_anchorLocalPosition = transform.localPosition;
-        m_anchorLocalRotation = transform.localRotation;
 
         m_groupRootXform = transform;
         while(m_groupRootXform.parent != null && m_groupRootXform.parent.gameObject.tag == gameObject.tag)
         {
             m_groupRootXform = m_groupRootXform.parent;
         }
+
+        m_parentXform= m_groupRootXform.parent;
+        m_anchorLocalPosition = m_groupRootXform.localPosition;
     }
 
     // Update is called once per frame
@@ -56,8 +58,6 @@ public class DragOnClick : MonoBehaviour
         var deltaMouse = (mousePos - m_lastMousePos) * m_mouseMovementScale;
         m_lastMousePos = mousePos;
 
-        Debug.Log(deltaMouse);
-
         if (m_groupRootXform != null) 
         {
             m_groupRootXform.position += Camera.main.transform.TransformDirection(deltaMouse);
@@ -67,5 +67,14 @@ public class DragOnClick : MonoBehaviour
     private void OnMouseUp()
     {
         m_isMoving = false;
+        if (m_parentXform != null)
+        {
+            float distance = Vector3.Distance(m_groupRootXform.position, m_parentXform.position);
+            if(distance < m_attachDistance)
+            {
+                m_groupRootXform.parent = m_parentXform;
+                m_groupRootXform.localPosition = m_anchorLocalPosition;
+            }
+        }
     }
 }
