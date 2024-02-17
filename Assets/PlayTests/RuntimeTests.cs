e2e6444a22c51ae0c3a278d1de23b5f9d4a52ec4
warrencouvillion <31316773+warrencouvillion@users.noreply.github.com>
2024-02-17 12:09:01 -0600

    Completed test of UFO look-at functionality.

--- Assets/PlayTests/RuntimeTests.cs
+++ Assets/PlayTests/RuntimeTests.cs
@@ -11,6 +11,7 @@ public class RunTest
 {
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
+
     [UnityTest]
     public IEnumerator TestUFOLookAtTarget()
     {
@@ -20,25 +21,35 @@ public class RunTest
 
         //Create mock input & time services
         var mockInput = Substitute.For<IInputInterface>();
+        //No movement on update.
+        mockInput.GetAxis("Vertical").Returns(0);
+        mockInput.GetAxis("Horizontal").Returns(0);
+        mockInput.GetKey(KeyCode.LeftShift).Returns(false);
+        mockInput.GetKey(KeyCode.RightShift).Returns(false);
+        //But simulate <HOME> key being pressed
+        mockInput.GetKeyDown(KeyCode.Home).Returns(true);
 
         //Create an object with a UFO movement
         GameObject ufoObj = new GameObject();
         var ufoComp = ufoObj.AddComponent<UFOMovement>();
+        ufoComp.m_input = mockInput;
+        ufoComp.m_target = target;
         
         //Position the ufoObj away from the origin
-        ufoObj.transform.position = new Vector3(0, 10, 0);
-        //Assert that the UFO is *not* looking at the target :w
+        ufoObj.transform.position = new Vector3(5, 0, 10);
+        //Assert that the UFO is *not* looking at the target 
         //(Testing the test)
-        Vector3 lookAtVec = ufoObj.transform.position - target.transform.position;
+        Vector3 lookAtVec = target.transform.position - ufoObj.transform.position;
         lookAtVec.Normalize();
         Assert.AreNotEqual(lookAtVec, ufoObj.transform.forward);
 
-        
+        //Give it time to rotate.
+        yield return new WaitForSeconds(1.0f);
 
-
-
-
-
-        yield return null;
+        lookAtVec = target.transform.position - ufoObj.transform.position;
+        lookAtVec.Normalize();
+        //Check that the ufo is now looking at the target. If it is, it's 
+        //forward vector should be pretty close to the lookAtVec calculated above.
+        Assert.AreEqual(Vector3.Dot(lookAtVec, ufoObj.transform.forward), 1.0, 0.01);
     }
 }
