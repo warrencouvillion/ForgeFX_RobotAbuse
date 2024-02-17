2f4376c987ca08bd7a69b6a2d292921260e3789e
warrencouvillion <31316773+warrencouvillion@users.noreply.github.com>
2024-02-17 09:29:03 -0600

    Split RobotInput into its own file.

--- Assets/Scripts/InputInterface.cs
+++ Assets/Scripts/InputInterface.cs
@@ -17,31 +17,3 @@ public interface IInputInterface
     public bool GetMouseButton(int button);
 }
 
-class RobotInput : IInputInterface
-{
-    public float GetAxis(string axisName)
-    {
-        return Input.GetAxis(axisName);
-    }
-
-    public bool GetKey(KeyCode key)
-    {
-        return Input.GetKey(key);
-    }
-
-    public Vector3 mousePosition
-    {
-        get { return Input.mousePosition; }
-    }
-
-    public bool GetKeyDown(KeyCode key)
-    {
-        return Input.GetKeyDown(key);
-    }
-
-    public bool GetMouseButton(int button)
-    {
-        return Input.GetMouseButton(button);
-    }
-}
-
---
+++ Assets/Scripts/RobotInput.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+class RobotInput : IInputInterface
+{
+    public float GetAxis(string axisName)
+    {
+        return Input.GetAxis(axisName);
+    }
+
+    public bool GetKey(KeyCode key)
+    {
+        return Input.GetKey(key);
+    }
+
+    public Vector3 mousePosition
+    {
+        get { return Input.mousePosition; }
+    }
+
+    public bool GetKeyDown(KeyCode key)
+    {
+        return Input.GetKeyDown(key);
+    }
+
+    public bool GetMouseButton(int button)
+    {
+        return Input.GetMouseButton(button);
+    }
+}
+
